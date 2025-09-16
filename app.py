1dd3984513bb58bba62a53424c92e2be270dfa74
Keerthi Vemula Murali <kmurali@wpi.edu>
2025-09-15 20:15:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ import gradio as gr
 
 class FSRCNN(nn.Module):
     def __init__(self, scale_factor, num_channels=1, d=56, s=12, m=4):
-        super(FSRCNN, self)._init_()
+        super(FSRCNN, self).__init__()
         self.first_part = nn.Sequential(
             nn.Conv2d(num_channels, d, kernel_size=5, padding=5 // 2),
             nn.PReLU(d)
